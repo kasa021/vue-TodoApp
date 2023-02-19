3096ced35eec24bf572c1b22a1321f90e9d21103
kasa021 <mkafa.k11@gmail.com>
2023-02-19 17:22:08 +0900

    雛形を作成

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {ref,reactive,computed, defineComponent} from 'vue'
+
+const data = () =>({
+
+})
+
+</script>
+
+<template>
+<h1> My Todo App</h1>
+<input type="text"/><button>Add</button><button>Delete</button>
+<ul>
+  <li>
+    <input type="checkbox"  checked/><span class="todo-done"
+    >Learn Vue</span>
+  </li>
+  <li><intput type ="checkbox"/><span> buy milk</span></li>
+  <li><intput type ="checkbox"/><span> buy eggs</span></li>
+</ul>
+</template>
+
+<style>
+body {
+  background-color: #eee;
+}
+
+.todo-done {
+  text-decoration: line-through;
+}
+</style>
