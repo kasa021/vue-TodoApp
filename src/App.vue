9e0efdcbac0870e7c2f9f1a21928a98439f007ea
kasa021 <mkafa.k11@gmail.com>
2023-02-19 18:12:29 +0900

    AddTodo関数を追加 inputに何もなかった場合alertを表示

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <script setup lang="ts">
 import {ref,reactive,computed, defineComponent} from 'vue'
 
-const data = () =>({
+interface Todo {
+  text: string;
+  done: boolean;
+}
+const todos = ref<Todo[]>([
+]);
 
-})
+let input = reactive<Todo>({
+  text: '',
+  done: false
+});
+let todoNum = computed(() => {
+  return todos.value.length;
+});
+const AddTodo = () => {
+  if(input.text == ''){
+    alert('Please enter a todo');
+    return;
+  }
+  todos.value.push(input);
+  input = reactive<Todo>({
+    text: '',
+    done: false
+  });
+};
+
+const DeleteTodo = () => {
+  todos.value.pop();
+};
 
 </script>
 
 <template>
 <h1> My Todo App</h1>
-<input type="text"/><button>Add</button><button>Delete</button>
-<ul>
-  <li>
-    <input type="checkbox"  checked/><span class="todo-done"
-    >Learn Vue</span>
-  </li>
-  <li><intput type ="checkbox"/><span> buy milk</span></li>
-  <li><intput type ="checkbox"/><span> buy eggs</span></li>
+<input type="text" v-model="input.text"/><button @click="AddTodo">Add</button><button @click="DeleteTodo">Delete</button>
+<p v-if="todoNum == 0">No todos</p>
+<ul v-else>
+  <li v-for = "todo in todos"><input type ="checkbox" /><span>{{ todo.text }}</span></li>
 </ul>
 </template>
 
